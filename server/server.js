2e5e5d43d0eff2bc90ebc49ff3a36bb6196edfa0
Matt Kilcup <nevermindthelabel@gmail.com>
2020-08-15 22:42:47 -0700

    updated server with mongo info

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const PORT = process.env.port || 3001;
-
-require('dotenv').config();
+const mongoose = require('mongoose');
+const mongoURI = process.env.mongoURI;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+console.log(mongoURI);
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+
+
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
 app.get('/', (req, res) => {
   res.json({ success: true })
